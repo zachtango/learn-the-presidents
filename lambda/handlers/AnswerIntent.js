46309f819048b03e289a69963434b3f2293fe4b1
tango <zachtangoj@gmail.com>
2021-08-31 14:54:09 -0500

    AnswerIntent chainsb ack to TestIntent to ask another question

--- lambda/handlers/AnswerIntent.js
+++ lambda/handlers/AnswerIntent.js
@@ -7,10 +7,29 @@ const AnswerIntentHandler = {
     },
     handle(handlerInput) {
 
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
         const presidentSlot = Alexa.getSlot(handlerInput.requestEnvelope, 'president');
         const presidentId = presidentSlot.resolutions.resolutionsPerAuthority[0].values[0].value.id;
+        const correct = true;
+
+        if(correct){ // check answer
+            sessionAttributes.test.questionNum += 1;    
+            
+        }
 
         return handlerInput.responseBuilder
+            .addDelegateDirective({
+                name: 'TestIntent',
+                confirmationStatus: 'NONE',
+                slots: {
+                    difficulty: {
+                        name: "difficulty",
+                        value: sessionAttributes.test.difficulty,
+                        confirmationStatus: "CONFIRMED"
+                    }
+                }
+            })
             .speak(presidentId)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
