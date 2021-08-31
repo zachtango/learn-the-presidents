8b94d06186b4b6a99061dfe3d5c61030cb8573cc
tango <zachtangoj@gmail.com>
2021-08-31 17:02:46 -0500

    debugging

--- lambda/handlers/TestIntent.js
+++ lambda/handlers/TestIntent.js
@@ -15,7 +15,7 @@ const StartedInProgressTestIntentHandler = {
         const DIFFICULTY = handlerInput.requestEnvelope.request.intent.slots.difficulty;
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
         const currentIntent = handlerInput.requestEnvelope.request.intent;
-        console.log(sessionAttributes.test);
+        console.log(currentIntent);
 
         if(DIFFICULTY){
             return handlerInput.responseBuilder
