27223b670e53c73edeb7407f88106138ec963cb5
tango <zachtangoj@gmail.com>
2021-08-31 14:41:27 -0500

    TestIntent logic implemented

--- lambda/handlers/TestIntent.js
+++ lambda/handlers/TestIntent.js
@@ -1,32 +1,45 @@
-const Alexa = require('ask-sdk-core');
-
 // Session attributes to persist throughout lifespan of current skill session
 const TestIntentHandler = {
     canHandle(handlerInput) {
         console.log('TEST INTENT CAN HANDLE');
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'TestIntent';
+
+        const request = handlerInput.requestEnvelope.request;
+        
+        console.log(request);
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'TestIntent';
     },
     handle(handlerInput) {
         console.log('TEST INTENT HANDLER');
+
         const DIFFICULTY = handlerInput.requestEnvelope.request.intent.slots.difficulty.value;
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
-        sessionAttributes.test = {
-            difficulty: DIFFICULTY,
-            questionNum: 1
-        };
+        let speakOutput;
+        if(!sessionAttributes.test){
+            // test just started
 
-        const speakOutput = 'Let me get that started for you';
+            sessionAttributes.test = {
+                difficulty: DIFFICULTY,
+                questionNum: 1
+            };
+
+            speakOutput = 'Let me get that started for you. Question 1';
+
+            
+
+        } else{
+            // test in progress
+            speakOutput = `Question ${sessionAttributes.test.questionNum}`;
+        }
 
         return handlerInput.responseBuilder
             .addDelegateDirective({
-                name: 'QuestionIntent',
+                name: 'AnswerIntent',
                 confirmationStatus: 'NONE',
                 slots: {}
             })
             .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
