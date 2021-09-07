b11e352bd34c0ef02df19a891154b910630f49c6
tango <zachtangoj@gmail.com>
2021-09-06 22:11:39 -0500

    resume test logic started

--- lambda/handlers/TestIntent.js
+++ lambda/handlers/TestIntent.js
@@ -5,7 +5,7 @@ const { genNormalProblems, genHardProblems } = require('../functions/presidentFu
 
 
 // Session attributes to persist throughout lifespan of current skill session
-const TestIntentHandler = {
+const StartTestIntentHandler = {
     canHandle(handlerInput) {
         console.log('STARTED TEST INTENT CAN HANDLE');
 
@@ -55,4 +55,54 @@ const TestIntentHandler = {
     }
 };
 
-module.exports = TestIntentHandler;
+const ResumeTestIntentHandler = {
+    canHandle(handlerInput) {
+        console.log('RESUME TEST INTENT CAN HANDLE');
+
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'TestIntent';
+    },
+    handle(handlerInput) {
+        console.log('RESUME TEST INTENT HANDLER');
+        const DIFFICULTY = Alexa.getSlot(handlerInput.requestEnvelope, 'difficulty').value;
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        
+        console.log(JSON.stringify(sessionAttributes));
+        // test just started
+        sessionAttributes.test = {
+            difficulty: DIFFICULTY,
+            questionNum: 0,
+            attempts: 0,
+            isRunning: true,
+            numCorrect: 0,
+            hintMessageGiven: false
+        };
+
+        console.log(JSON.stringify(sessionAttributes));
+
+        const test = sessionAttributes.test;
+        console.log(DIFFICULTY);
+        if(DIFFICULTY === 'normal'){
+
+            test.problems = genNormalProblems();
+        
+        } else if(DIFFICULTY === 'hard'){
+            
+            test.problems = genHardProblems();
+
+        } else{
+            console.log('invalid value in DIFFICULTY');
+            return 0;
+        }
+        
+        const speakOutput = test.problems[0].question;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+module.exports = StartTestIntentHandler;
