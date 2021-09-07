0c362565e63a0218a7bc24189bd7f92c9270a49b
tango <zachtangoj@gmail.com>
2021-09-06 21:45:25 -0500

    fixed bugs w/ ResponseInterceptor interfering w/ AnswerIntent

--- lambda/index.js
+++ lambda/index.js
@@ -152,8 +152,8 @@ const SaveAttributesResponseInterceptor = {
         if (!response) return; // avoid intercepting calls that have no outgoing response due to errors
         const {attributesManager, requestEnvelope} = handlerInput;
         const sessionAttributes = attributesManager.getSessionAttributes();
-        const shouldEndSession = (typeof response.shouldEndSession === "undefined" ? true : response.shouldEndSession); //is this a session end?
-        if (shouldEndSession || Alexa.getRequestType(requestEnvelope) === 'SessionEndedRequest') { // skill was stopped or timed out
+        //const shouldEndSession = (typeof response.shouldEndSession === "undefined" ? true : response.shouldEndSession); //is this a session end?
+        if (Alexa.getRequestType(requestEnvelope) === 'SessionEndedRequest') { // skill was stopped or timed out
             // we increment a persistent session counter here
             sessionAttributes['sessionCounter'] = sessionAttributes['sessionCounter'] ? sessionAttributes['sessionCounter'] + 1 : 1;
             // we make ALL session attributes persistent
