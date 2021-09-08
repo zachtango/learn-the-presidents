11e89a1a4326142c6bd811aaae1b4870caf6e07c
Nathaniel Brown <nateabrown@icloud.com>
2021-09-08 15:41:21 -0500

    add functionality for president of the day intent

--- lambda/handlers/PresOfDayIntent.js
+++ lambda/handlers/PresOfDayIntent.js
@@ -1,4 +1,5 @@
 const Alexa = require('ask-sdk-core');
+const { getPresidentOfDayResponse } = require("../functions/getPresidentOfDay");
 
 const PresOfDayIntentHandler = {
     canHandle(handlerInput) {
@@ -6,11 +7,25 @@ const PresOfDayIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'PresOfDayIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Hello World! Test';
+        // Getting user's time zone
+        const serviceClientFactory = handlerInput.serviceClientFactory;
+        const deviceId = handlerInput.requestEnvelope.context.System.device.deviceId;
+        let userTimeZone;
+        try {
+            const upsServiceClient = serviceClientFactory.getUpsServiceClient();
+            userTimeZone = await upsServiceClient.getSystemTimeZone(deviceId);
+        } catch (error) {
+            if (error.name !== 'ServiceError') {
+                return handlerInput.responseBuilder.speak("There was a problem connecting to the service").getResponse();
+            }
+            console.log('error', error.message);
+        }
+        console.log(`User time zone: ${userTimeZone}`);
+        // Generate response accordingly
+        const response = getPresidentOfDayResponse(userTimeZone);
 
         return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .speak(response)
             .getResponse();
     }
 };
